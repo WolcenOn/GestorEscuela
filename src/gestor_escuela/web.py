dac8b00504e20dbb6d3ecb3f5e8a37b29180a42f
WolcenOn <fhaldon@gmail.com>
2026-08-18 16:18:16 +0100

    Expose academic workflow routes in web app

--- src/gestor_escuela/web.py
+++ src/gestor_escuela/web.py
@@ -4,9 +4,12 @@ from pathlib import Path
 
 from fastapi.responses import FileResponse
 
+from gestor_escuela.api.academic import router as academic_router
 from gestor_escuela.api.app import app
 
 _UI_FILE = Path(__file__).with_name("static") / "index.html"
+
+app.include_router(academic_router)
 
 
 @app.get("/", include_in_schema=False)
